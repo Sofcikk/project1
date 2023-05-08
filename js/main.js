323555908f4c19fb03f02400ff047f6b3e874aad
Zosia Samosia <Zosia@Samosia.pl>
2023-05-08 12:44:39 +0200

    mumi

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 'use scrict';
 
-function selectVehicle() {
+const btn = document.querySelector('header a');
+
+const h1 = document.querySelector('header h1');
+
+const h1Text = h1.innerText;
+
+let counter = 0;
+
+btn.addEventListener('click', () =>{
+    counter ++;
+
+    // if(counter % 2 === 0) {
+    //     h1.innerText = h1Text;
+    // }else {
+    //     h1.innerText = 'Będzie Pan zadowolony ;)';
+    // }
     
-}
+    counter % 2 === 0 ? h1.innerText = h1Text : h1.innerText = "bedzie pan zadowolony ;)"
+
+});
